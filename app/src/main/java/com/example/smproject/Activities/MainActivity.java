e57b19fc0217f0d8eba28e75919142d3e1f8ee07
WitoldPietrzak <witekpietrzak35@gmail.com>
2020-12-13 02:46:48 +0100

    Created HealthAnalizer and BreathChecker

---
+++ app/src/main/java/com/example/smproject/Activities/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.smproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.testTasks).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToTasksTest();
+            }
+        });
+    }
+
+
+    public void goToTasksTest()
+    {
+        Intent intent = new Intent(this,TaskTestsActivity.class);
+        startActivity(intent);
+    }
+}
